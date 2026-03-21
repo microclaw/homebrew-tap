036268cd79f97965eb991701e1c8ee59079c786f
everettjf <everettjf@live.com>
2026-03-20 22:04:36 -0700

    microclaw homebrew release 0.1.27

--- Formula/microclaw.rb
+++ Formula/microclaw.rb
@@ -1,9 +1,17 @@
 class Microclaw < Formula
   desc "Agentic AI assistant for Telegram - web search, scheduling, memory, tool execution"
   homepage "https://github.com/microclaw/microclaw"
-  url "https://github.com/microclaw/microclaw/releases/download/v0.1.25/microclaw-0.1.25-aarch64-apple-darwin.tar.gz"
-  sha256 "d15ed5eab424eaef185618bb93b186cb75452cdf5759ea522e50defbdc12a587"
   license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/microclaw/microclaw/releases/download/v0.1.27/microclaw-0.1.27-aarch64-apple-darwin.tar.gz"
+      sha256 "421773e911814b8b81754bbb86f41f4011d75cc4a8da8d37d006c42ee14d27c7"
+    else
+      url "https://github.com/microclaw/microclaw/releases/download/v0.1.27/microclaw-0.1.27-x86_64-apple-darwin.tar.gz"
+      sha256 "30dd2eea825bd0f383ae6e343eddfcf50b9bce6dd2c1c77b16180c19863e55ca"
+    end
+  end
 
   def install
     bin.install "microclaw"
