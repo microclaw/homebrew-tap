78a9f3aec2fd8c3a2a80dce269c0c419d4057a50
everettjf <everettjf@live.com>
2026-03-24 21:08:29 -0700

    microclaw homebrew release 0.1.29

--- Formula/microclaw.rb
+++ Formula/microclaw.rb
@@ -5,11 +5,11 @@ class Microclaw < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/microclaw/microclaw/releases/download/v0.1.28/microclaw-0.1.28-aarch64-apple-darwin.tar.gz"
-      sha256 "894eee78c440066a71fa19aa8c931887b7a1a44c90dbb497b5bfd5b67b6c4f4b"
+      url "https://github.com/microclaw/microclaw/releases/download/v0.1.29/microclaw-0.1.29-aarch64-apple-darwin.tar.gz"
+      sha256 "35c400be195dcf091553c55b3b78e91967645b8f454ba9c067e2663a0803e37d"
     else
-      url "https://github.com/microclaw/microclaw/releases/download/v0.1.28/microclaw-0.1.28-x86_64-apple-darwin.tar.gz"
-      sha256 "166dbf88673dd4ba2baf27d98f77117f7c303ed901ee68b7c00fdc7f58ed96ec"
+      url "https://github.com/microclaw/microclaw/releases/download/v0.1.29/microclaw-0.1.29-x86_64-apple-darwin.tar.gz"
+      sha256 "f8377dafbeff22bc86ad8f8fac84ee2c17e079dc01fd2c6acbbd7a8f208add53"
     end
   end
 
