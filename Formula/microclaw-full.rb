022a91f23e433851785a1ab9041fedb69d8997c2
everettjf <everettjf@live.com>
2026-05-26 10:02:55 -0700

    microclaw homebrew release 0.1.59

--- Formula/microclaw-full.rb
+++ Formula/microclaw-full.rb
@@ -5,11 +5,11 @@ class MicroclawFull < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/microclaw/microclaw/releases/download/v0.1.58/microclaw-full-0.1.58-aarch64-apple-darwin.tar.gz"
-      sha256 "bbab10bb7d8b3b463368d3303e4057d448a24e7b97d9aa8a7806b80c1afc35b8"
+      url "https://github.com/microclaw/microclaw/releases/download/v0.1.59/microclaw-full-0.1.59-aarch64-apple-darwin.tar.gz"
+      sha256 "e02bd12710df6481d547dee32adb195469da28c61b7336ac1a4a5eb690ded7ff"
     else
-      url "https://github.com/microclaw/microclaw/releases/download/v0.1.58/microclaw-full-0.1.58-x86_64-apple-darwin.tar.gz"
-      sha256 "051ac313d5d77c7fab96cc21693925d03e2702b0e789dafce41f6f6f13cd8bee"
+      url "https://github.com/microclaw/microclaw/releases/download/v0.1.59/microclaw-full-0.1.59-x86_64-apple-darwin.tar.gz"
+      sha256 "5304375825620d69e2a6235510ba37290539aa86f859717c844e8eafb68987bd"
     end
   end
 
--- Formula/microclaw.rb
+++ Formula/microclaw.rb
@@ -5,11 +5,11 @@ class Microclaw < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/microclaw/microclaw/releases/download/v0.1.58/microclaw-0.1.58-aarch64-apple-darwin.tar.gz"
-      sha256 "72936d5dec943b4b957b4132159eb321e4afdbcf692f9bb0864b1842a4e3ff29"
+      url "https://github.com/microclaw/microclaw/releases/download/v0.1.59/microclaw-0.1.59-aarch64-apple-darwin.tar.gz"
+      sha256 "a1e84c12c3bb2453bb7cf1c6b7a9069b8fda0d0cf6b87327db7675e66fd80b49"
     else
-      url "https://github.com/microclaw/microclaw/releases/download/v0.1.58/microclaw-0.1.58-x86_64-apple-darwin.tar.gz"
-      sha256 "fabc40b13f5e4509df9dd772e4a1c7f884dc33cbf61903af02f04c0829d0f4b7"
+      url "https://github.com/microclaw/microclaw/releases/download/v0.1.59/microclaw-0.1.59-x86_64-apple-darwin.tar.gz"
+      sha256 "30f287ef1d1015ecfb57364cceae34554c3ee1aac24e594152fb47043a603c5d"
     end
   end
 
